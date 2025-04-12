5cc0727b85d461483fbc967e867575bf3519fa46
Mikołaj Kokot <panmeek@proton.me>
2025-04-12 21:45:54 +0200

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function getComputerChoice() {
+  let randomNumber = Math.ceil(Math.random() * 3)
+  if (randomNumber === 1) {
+    return "rock"
+  } else if (randomNumber === 2) {
+    return "paper"
+  } else {
+    return "scissors"
+  }
+}
