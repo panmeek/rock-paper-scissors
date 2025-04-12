0f6340bba845ed329095df0b90e5b10f59abaf0a
Mikołaj Kokot <panmeek@proton.me>
2025-04-12 21:54:16 +0200

    Add getHumanChoice function, add score variables

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0
+let humanScore = 0
+
 function getComputerChoice() {
   let randomNumber = Math.ceil(Math.random() * 3)
   if (randomNumber === 1) {
@@ -7,4 +10,8 @@ function getComputerChoice() {
   } else {
     return "scissors"
   }
+}
+
+function getHumanChoice() {
+  return prompt("Rock, paper or scissors?").toLowerCase()
 }
