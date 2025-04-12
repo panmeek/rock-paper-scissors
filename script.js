0a1fd813e7aa6af0e533a978a050ef20b2e08706
Mikołaj Kokot <panmeek@proton.me>
2025-04-12 23:06:19 +0200

    Add playGame function, improve readability
    Moved the whole code to the playGame function as the other functions
    and variables won't be used outside of it.
    
    Changed indentation to tab which improves readability and is in the
    spirit of my other pieces of code.

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
-let computerScore = 0;
-let humanScore = 0;
+function playGame() {
+	let computerScore = 0;
+	let humanScore = 0;
 
-function getComputerChoice() {
-  let randomNumber = Math.ceil(Math.random() * 3);
-  if (randomNumber === 1) {
-    return "rock";
-  } else if (randomNumber === 2) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
-}
+	function getComputerChoice() {
+		let randomNumber = Math.ceil(Math.random() * 3);
+		if (randomNumber === 1) {
+			return "rock";
+		} else if (randomNumber === 2) {
+			return "paper";
+		} else {
+			return "scissors";
+		}
+	}
 
-function getHumanChoice() {
-  return prompt("Rock, paper or scissors?").toLowerCase();
-}
+	function getHumanChoice() {
+		return prompt("Rock, paper or scissors?").toLowerCase();
+	}
 
-function playRound(humanChoice, computerChoice) {
-  if (
-    humanChoice === "rock" && computerChoice === "scissors"
-    || humanChoice === "paper" && computerChoice === "rock"
-    || humanChoice === "scissors" && computerChoice === "paper"
-  ) {
-    console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
-    humanScore++;
-  } else if (humanChoice === computerChoice) {
-    console.log("It's a draw!");
-    return;
-  } else {
-    console.log(`You lose, ${computerChoice} beats ${humanChoice}!`);
-    computerScore++;
-  }
+	function playRound(humanChoice, computerChoice) {
+		if (
+			humanChoice === "rock" && computerChoice === "scissors"
+			|| humanChoice === "paper" && computerChoice === "rock"
+			|| humanChoice === "scissors" && computerChoice === "paper"
+		) {
+			console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+			humanScore++;
+		} else if (humanChoice === computerChoice) {
+			console.log("It's a draw!");
+			return;
+		} else {
+			console.log(`You lose, ${computerChoice} beats ${humanChoice}!`);
+			computerScore++;
+		}
+	}
+
+	for (let i = 0; i < 5; i++) {
+		playRound(
+			getHumanChoice(),
+			getComputerChoice()
+		);
+	}
+	console.log(
+		`Game Score\nPlayer: ${humanScore}\nComputer: ${computerScore}`
+	);
 }
